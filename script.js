7ef1d275300d283a13c91a67d2ee142ff0847913
M. Manousos <mmanousos@gmail.com>
2018-01-10 15:59:21 -0800

    replaces 'clear-entry' button with decimal point

--- script.js
+++ script.js
@@ -20,46 +20,6 @@ $(document).ready(function() {
         $('#display-main').html("");
     });
     
-    // push button values into an array? 
-    
-  /*  
-    
-    $('.number, .calc, #enter').on('click', function() { 
-        var buttonValue = $(this).find('.text').html();
-        var btnValStr = buttonValue.toString();
-        
-        // if number button is pressed    
-        if ($(this).is('.number')) {
-            console.log("buttonValue is " + buttonValue);
-            // add number value to holdValue
-            holdValue += btnValStr; 
-            console.log("holdValue is " + holdValue);
-            
-            // add number value to runningValue 
-            runningValue.push(buttonValue);
-            console.log("runningValue is " + runningValue);
-            
-        } else if ($(this).is('.calc')) {
-            console.log("buttonValue is " + buttonValue);
-            // hold operator in it's own variable 
-            holdOperator = btnValStr;
-            console.log("holdOperator is " + holdOperator);
-        
-            // add operator value to runningValue 
-            runningValue.push(buttonValue); 
-            console.log("runningValue is " + runningValue);
-        
-        } else if ($(this).is('#enter')) {
-            //combine runningValue to generate finalValue
-            finalValue = runningValue.join('');
-            console.log(eval(finalValue));
-
-            // display calculated finalValue
-            $('#display-main').html('' + finalValue + '');
-            
-        }
-    });
-    */
     // 'clear-entry' button functionality
     $('#clear-entry').on('click', function() {
         holdValue = '';
